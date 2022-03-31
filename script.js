0d5ac8a83ce00105c8eb6435c7dd299208892aa0
klmitchy <karenapolis@gmail.com>
2022-03-31 14:57:59 -0500

    trying time function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//test
+//function ShowMessage() {
+ //   alert("Hello World!");
+//}
+
+//ShowMessage();
 
 //set current day and time in header
-var currentDate = moment().format('LLLL');
-$("#currentDay").text(currentDate);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY');
 
-//
+//grab current hour of day to determine past, present, future:
+var currentTime = moment().hours();
+
+//add evvent listener for save button:
+$(".saveBtn")onabort("click", function() {
+    var textValue = $(this).siblings("color-block".value)
+
+})
