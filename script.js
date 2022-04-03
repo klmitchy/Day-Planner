fbc715d5f0c8cee5ac8e398bcee3cb8b77ab7fa8
klmitchy <karenapolis@gmail.com>
2022-04-03 14:31:20 -0500

    more css, re-do forms line from bootstrap

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-//test
-//function ShowMessage() {
- //   alert("Hello World!");
-//}
-
-//ShowMessage();
 
 //set current day and time in header
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY');
+var todaysDate = $("#currentDay");
+
+var todaysDate= moment().format('dddd MMMM Do YYYY');
+$("currentDay").text(todaysDate);
+
 
 //grab current hour of day to determine past, present, future:
+
 var currentTime = moment().hours();
 
 //add evvent listener for save button:
+
+var saveActivity = $(".saveBtn")
+
 $(".saveBtn")onabort("click", function() {
+ 
     var textValue = $(this).siblings("color-block".value)
 
 })
+
